5db1d73f78b0eea3798fb82abebfa69d4609473a
kantapong_int <kantapong.int.work@gmail.com>
2024-03-06 22:39:03 +0700

    test commit

---
+++ message/src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './message.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepo: MessageRepository) {}
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
---
+++ scratch/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("/api")
+export class AppController {
+    @Get("/hi")
+    getRootRoute(){
+        return "hi there"
+    }
+
+    @Get('/bye')
+    getByeThere(){
+        return "bye there"
+    }
+}
